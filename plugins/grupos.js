4770c06b1fe986e7ba7d5cf24db290c94ef036f6
Andreayt0972 <89881184+Andreayt0972@users.noreply.github.com>
2022-05-03 19:09:07 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,13 +3,13 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The ANDREA YT - Bot*
 
 *➤ Grupos oficiales del Bot:*
 1 https://chat.whatsapp.com/IYl8F2b6XIh1YIZmO0w0va
 `.trim() 
 
-conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE ANDREA YT - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu.jpg`)}}}})}
 
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
